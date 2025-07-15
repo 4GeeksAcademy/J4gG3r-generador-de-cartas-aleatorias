b74d7e52d66d3b5def26ffe454222bd724692a1f
Johan Gomez <johangomez0612@gmail.com>
2025-07-15 01:46:32 +0000

    generador de cartas aleatorio listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let numbers = ["2", "3", "4", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"]
+  let icons = ["♥", "♠", "♣", "♦"]
+
+  const generateRandomValue = (array) => {
+    return array[Math.floor(Math.random() * array.length)]
+  }
+
+  let randomNumber = generateRandomValue(numbers)
+  let randomIcon = generateRandomValue(icons)
+
+
+  const elements = document.querySelectorAll("span")
+  elements[0].innerText=randomIcon
+  elements[1].innerText=randomNumber
+  elements[2].innerText=randomIcon
+
 };
